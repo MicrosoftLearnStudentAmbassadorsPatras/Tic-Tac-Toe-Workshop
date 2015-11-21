b1ca73db54d3a38b0cf30ceefdcb9f0ea7286f19
Gerasimos Alexiou <webmaster@geralexgr.com>
2015-11-21 18:27:17 +0200

    initial commit

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.b9);
+            this.panel1.Controls.Add(this.b8);
+            this.panel1.Controls.Add(this.b7);
+            this.panel1.Controls.Add(this.b6);
+            this.panel1.Controls.Add(this.b5);
+            this.panel1.Controls.Add(this.b4);
+            this.panel1.Controls.Add(this.b3);
+            this.panel1.Controls.Add(this.b2);
+            this.panel1.Controls.Add(this.b1);
+            this.panel1.Location = new System.Drawing.Point(12, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(455, 269);
+            this.panel1.TabIndex = 0;
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.Location = new System.Drawing.Point(305, 181);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(145, 83);
+            this.b9.TabIndex = 8;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.Location = new System.Drawing.Point(154, 181);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(145, 83);
+            this.b8.TabIndex = 7;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.Location = new System.Drawing.Point(3, 181);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(145, 83);
+            this.b7.TabIndex = 6;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.Location = new System.Drawing.Point(305, 92);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(145, 83);
+            this.b6.TabIndex = 5;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.Location = new System.Drawing.Point(154, 92);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(145, 83);
+            this.b5.TabIndex = 4;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.Location = new System.Drawing.Point(3, 92);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(145, 83);
+            this.b4.TabIndex = 3;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(305, 3);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(145, 83);
+            this.b3.TabIndex = 2;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(154, 3);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(145, 83);
+            this.b2.TabIndex = 1;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(3, 3);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(145, 83);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(166, 375);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(145, 48);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "Start New Game";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Start_New_Game);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(141, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Next Turn: Player 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 436);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private int turn;
+        private string symbol;
+        private string[,] board;
+        private bool gameOver;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            turn = 2;
+            board = new String[3, 3] { { "1", "2", "3" },
+                                       { "4", "5", "6",},
+                                       { "7", "8", "9" }
+                                     };
+            /* The Board:
+            *   [0,0] [0,1] [0,2]   ||  [b1] [b2] [b3]
+            *   [1,0] [1,1] [1,2]   ||  [b4] [b5] [b6]
+            *   [2,0] [2,1] [2,2]   ||  [b7] [b8] [b9]
+            */
+            symbol = "X";
+            gameOver = false;
+            
+        }
+
+        private void Start_New_Game(object sender, EventArgs e)
+        {
+            // Reset text on buttons
+            b1.Text = "";
+            b2.Text = "";
+            b3.Text = "";
+            b4.Text = "";
+            b5.Text = "";
+            b6.Text = "";
+            b7.Text = "";
+            b8.Text = "";
+            b9.Text = "";
+
+            // Reset Board
+            board = new String[3, 3] { { "1", "2", "3" },
+                                       { "4", "5", "6",},
+                                       { "7", "8", "9" }
+                                     };
+
+            label1.Text = "Next Turn: Player 1"; // Reset label
+            turn = 2; // Reset counter
+            symbol = "X"; // Reset symbol 
+            gameOver = false;
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (gameOver == false)
+            {
+                Button b = sender as Button;
+                // Determine symbol to place on button
+                if (turn % 2 == 0) // Player 1
+                {
+                    b.Text = "X";
+                    symbol = "X";
+                }
+                else  // Player 2
+                {
+                    b.Text = "O";
+                    symbol = "O";
+                }
+
+                updateBoard(b.Name);
+
+                turn++; // Next Player
+                label1.Text = "Next Turn: Player " + (turn % 2 + 1); // Update label
+            }
+
+            checkWin();
+
+            
+        }
+
+        private void updateBoard(string name)
+        {
+            // Update board
+            switch (name)
+            {
+                case "b1":
+                    board[0,0] = symbol;
+                    break;
+                case "b2":
+                    board[0,1] = symbol;
+                    break;
+                case "b3":
+                    board[0,2] = symbol;
+                    break;
+                case "b4":
+                    board[1,0] = symbol;
+                    break;
+                case "b5":
+                    board[1,1] = symbol;
+                    break;
+                case "b6":
+                    board[1,2] = symbol;
+                    break;
+                case "b7":
+                    board[2,0] = symbol;
+                    break;
+                case "b8":
+                    board[2,1] = symbol;
+                    break;
+                case "b9":
+                    board[2,2] = symbol;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void checkColumns()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if ( (board[0,i] == board[1,i])&&(board[1,i] == board[2,i]) )
+                {
+                    showWinMessage();
+                    break;
+                }
+            }
+        }
+
+        private void checkRows()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if ((board[i,0] == board[i,1]) && (board[i,1] == board[i,2]))
+                {
+                    showWinMessage();
+                    break;
+                }
+            }
+        }
+
+        private void checkDiag()
+        {
+            if ((board[0,0] == board[1,1]) && (board[1,1] == board[2,2]))
+            {
+                showWinMessage();
+            }else if ((board[0,2] == board[1,1]) && (board[1,1] == board[2,0]))
+            {
+                showWinMessage();
+            }
+        }
+
+        private void showWinMessage()
+        {
+            if(symbol == "X")
+            {
+                label1.Text = "Player 1 WINS!";
+            }else
+            {
+                label1.Text = "Player 2 WINS";
+            }
+
+            gameOver = true;
+        }
+
+        private void checkWin()
+        {
+            checkColumns();
+            checkRows();
+            checkDiag();
+        }
+
+    }
+}
